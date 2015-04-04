ae8083b8f797399d339db6db25faf92c4fcf939a
Stanislav Yadykin <syadykin@conversionscience.co.uk>
2015-04-04 23:10:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+= LDAP synchronize
---
+++ bin/ldap-sync.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+/*jslint node:true */
+'use strict';
+
+var extend   = require('extend'),
+    // ldapSync = require('ldap-sync'),
+    ldapSync = require('../lib'),
+    config   = {};
+
+try {
+  extend(true, config, require('/etc/ldap-sync.json'));
+} catch(e) {}
+
+try {
+  extend(true, config, require('../ldap-sync.json'));
+} catch(e) {}
+
+ldapSync(config);
